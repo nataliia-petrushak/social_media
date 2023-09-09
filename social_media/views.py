45f0533a3d82c772220000aa6607e09eb087386e
Amirammi <amirammina@gmail.com>
2023-09-09 18:24:00 +0300

    Added CreateUserView, UserViewSet & ManageUserView

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    # authentication_classes = (JWTAuthentication,)
+    # permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
