767f579bc33e306e9e55499aaf4a8c4277fb6a39
Amirammi <amirammina@gmail.com>
2023-09-10 17:37:18 +0300

    Added HashtagViewSet, PostViewSet fith filters by title, liked, authors, hashtag

--- social_media/views.py
+++ social_media/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Exists, OuterRef
 from rest_framework import generics, mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -7,7 +8,19 @@ from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, Bl
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from .serializers import UserSerializer
+from models import Post, Hashtag, Like, Comment
+from .serializers import (
+    UserSerializer,
+    UserListSerializer,
+    UserDetailSerializer,
+    PostSerializer,
+    PostListSerializer,
+    PostDetailSerializer,
+    CommentSerializer,
+    HashtagSerializer,
+    HashtagListSerializer,
+    HashtagDetailSerializer
+)
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -21,6 +34,23 @@ class UserViewSet(
 ):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        """Retrieve users with filters"""
+        username = self.request.query_params.get("username")
+
+        queryset = self.queryset
+
+        if username:
+            queryset = queryset.filter(username__icontains=username)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return UserListSerializer
+        if self.action == "retrieve":
+            return UserDetailSerializer
 
 
 class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
@@ -45,3 +75,61 @@ class APILogoutView(APIView):
         token = RefreshToken(token=refresh_token)
         token.blacklist()
         return Response({"status": "OK, goodbye"})
+
+
+class HashtagViewSet(viewsets.ModelViewSet):
+    queryset = Hashtag.objects.all()
+    serializer_class = HashtagSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return HashtagListSerializer
+        if self.action == "retrieve":
+            return HashtagDetailSerializer
+        return self.serializer_class
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.select_related("author").prefetch_related("hashtags")
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        queryset = self.queryset
+        user = self.request.user
+
+        hashtag = self.request.query_params.get("hashtag")
+        title = self.request.query_params.get("title")
+        liked_posts = self.request.query_params.get("liked")
+        authors = self.request.query_params.get("authors")
+
+        if authors:
+            if authors == "me":
+                queryset = queryset.filter(author=self.request.user)
+            if authors == "followings":
+                following_users = self.request.user.followings.all()
+                queryset = queryset.filter(author__in=following_users)
+
+        if hashtag:
+            queryset = queryset.filter(hashtags__name__icontains=hashtag)
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if liked_posts:
+            queryset = queryset.filter(likes__liker=user)
+
+        return queryset.annotate(
+            is_liked=Exists(
+                Like.objects.filter(liker=self.request.user, post_id=OuterRef("pk"))
+            )
+        )
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+        if self.action == "retrieve":
+            return PostDetailSerializer
+        return self.serializer_class
